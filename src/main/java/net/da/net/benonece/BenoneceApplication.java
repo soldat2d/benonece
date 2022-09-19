abfe6c69cb078bfe516ec494b85a50e5128ae5a7
soldat2d <ten.soldat@gmail.com>
2022-09-20 00:24:33 +0300

    start

---
+++ src/main/java/net/da/net/benonece/BenoneceApplication.java
@@ -0,0 +1,13 @@
+package net.da.net.benonece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenoneceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BenoneceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/da/net/benonece/BenoneceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.da.net.benonece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenoneceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
